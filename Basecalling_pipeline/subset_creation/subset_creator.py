1f6f5e1951ef8dfd0b60d547b2a3067556e05ac1
RodTol <rodolfo.tolloi@gmail.com>
2024-06-12 14:01:50 +0200

    Now the function will add until it surpasses the target size or the list is finished

--- Basecalling_pipeline/subset_creation/subset_creator.py
+++ Basecalling_pipeline/subset_creation/subset_creator.py
@@ -16,39 +16,39 @@ class Subsetter:
         self.samplesheet = samplesheet
         self.dataframe = pd.DataFrame(samplesheet.data)
 
-    def create_batch(self, target_size=1):
+    def create_batch(self, target_size=0.8):
         '''
         This function will take the files that needs to be processed
-        and create a list of maximum the size. Note that it is coded
-        in such a way that the limit cannot be surpassed in any way.
+        and create a list of maximum the size.
+        Files will be appended until the target size is reached, or the list
+        of "to-be-processed" files is ended.
         Every time a file is added to the batch, its basecalled status
-        changes to 'in progress'
+        changes to 'in progress'. This update will be processed to the
+        json file only if everything went right 
 
         It will return a list of dict. 
         '''
         all_non_basecalled_files = self.dataframe[self.dataframe['basecalled'] == False]
-        
         cumulative_size = 0
         batch = []
 
         for i, row in all_non_basecalled_files.iterrows():
             file_size = row['size(GB)']
-            if cumulative_size + file_size <= target_size:
-                #Check
-                if self._check_file_exist(row):
-                    #Add
-                    batch.append(row.to_dict())
-                    #Update
-                    self.dataframe.loc[i, 'basecalled'] = 'In progress'
-                    cumulative_size += file_size
-                else: 
-                    # Here we can modify the handling of this situation
-                    print('WARNING: error in locating the files')
-                    sys.exit(1) 
-            else:
+            #Check
+            if self._check_file_exist(row):
+                #Add
+                batch.append(row.to_dict())
+                #Update
+                self.dataframe.loc[i, 'basecalled'] = 'In progress'
+                cumulative_size += file_size
+            else: 
+                # Here we can modify the handling of this situation
+                print('WARNING: error in locating the files')
+                sys.exit(1) 
+            if cumulative_size >= target_size: 
                 break
 
-        #Update the samplesheet file
+        #Update the samplesheet file (every file was verified)
         self.samplesheet.data = self.dataframe.to_dict(orient='records')
         self.samplesheet.update_json_file()
 
