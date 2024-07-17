622b16dcaef820251318f334136098a768ba00de
RodTol <rodolfo.tolloi@gmail.com>
2024-07-17 18:09:54 +0200

    made cleaning stage optional

--- Basecalling_pipeline/monitor_run/script.sh
+++ Basecalling_pipeline/monitor_run/script.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=1
-#SBATCH --output=%x-%j.out
-#SBATCH --error=%x-%j.err
+##SBATCH --output=%x-%j.out
+##SBATCH --error=%x-%j.err
 
 pathToSamplesheet=$1
 RUN_PARAMS_PATH=$2
@@ -19,10 +19,3 @@ python3 main.py ${RUN_PARAMS_PATH} ${pathToSamplesheet}
 # Kill the Python process
 kill $python_pid
 
-# Check if the process was successfully killed
-if ps -p $python_pid > /dev/null
-then
-   echo "Process $python_pid could not be killed."
-else
-   echo "Process $python_pid has been killed successfully."
-fi
