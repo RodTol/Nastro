9a838d994817b8b86b87752cf7a1a86dcaf86f60
RodTol <rodolfo.tolloi@gmail.com>
2024-07-17 17:48:57 +0200

    reworked job handling for telegram

--- Basecalling_pipeline/monitor_run/script.sh
+++ Basecalling_pipeline/monitor_run/script.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=Monitor
 #SBATCH --time=3:0:0
-#SBATCH -A lage
-#SBATCH -p EPYC
-#SBATCH -N 1
-#SBATCH -n 2
-#SBATCH -c 1
-#SBATCH -mem=5GB
+#SBATCH --partition=EPYC
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=1
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 
 pathToSamplesheet=$1
 RUN_PARAMS_PATH=$2
 
-python3 /u/area/jenkins_onpexp/LTS_tolloi/Orfeo_bot/main.py \'${pathToSamplesheet}\' &
+python3 /u/area/jenkins_onpexp/LTS_tolloi/Orfeo_bot/main.py ${pathToSamplesheet} &
 pid=$!
 
-python3 main.py ${RUN_PARAMS_PATH} \'${pathToSamplesheet}\' 
+python3 main.py ${RUN_PARAMS_PATH} ${pathToSamplesheet} 
 
 # Kill the Python process
 kill $python_pid
