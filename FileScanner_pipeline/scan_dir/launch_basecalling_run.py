4ec5048227d77bb42c4fe5137d6f1f8c24a67b65
RodTol <rodolfo.tolloi@gmail.com>
2024-08-02 14:06:58 +0200

    Working file scanner pipeline. Able to create and dispatch samplesheet or to append to already existing one

---
+++ FileScanner_pipeline/scan_dir/launch_basecalling_run.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from Basecalling_pipeline.subset_creation.pipelineInteract import Jenkins_trigger
+from Basecalling_pipeline.samplesheet_check.samplesheet_api import Samplesheet
+
+def launch_run(samplesheet: Samplesheet):
+    #Create input, logs, output dirs
+    input_path = os.path.join(samplesheet.get_metadata()["outputLocation"], 'input')
+    logs_path = os.path.join(samplesheet.get_metadata()["outputLocation"], 'logs')
+    output_path = os.path.join(samplesheet.get_metadata()["outputLocation"], 'output')
+
+    os.makedirs(input_path, exist_ok=True)
+    os.makedirs(logs_path, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
+
+    jenkins_parameter =  {
+            "pathToSamplesheet": samplesheet.file_path,
+            "pathToInputDir" : input_path,
+            "pathToOutputDir": output_path,
+            "pathToLogsDir": logs_path,
+            "RUN_TESTING_CLEANUP": True
+        }    
+    
+    jenkins = Jenkins_trigger()
+    jenkins.start_job('tolloi/Pipeline_long_reads/basecalling_pipeline', 'kuribo', jenkins_parameter)
--- FileScanner_pipeline/scan_dir/main.py
+++ FileScanner_pipeline/scan_dir/main.py
@@ -1,8 +1,13 @@
 import sys
-from create_samplesheet import *
+import os
+from datetime import datetime
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from Basecalling_pipeline.subset_creation.pipelineInteract import Jenkins_trigger
+from create_samplesheet import *
+from launch_basecalling_run import launch_run
+from Basecalling_pipeline.monitor_run.bot_telegram import telegram_send_bar
+from Basecalling_pipeline.monitor_run.bot_telegram import telegram_send_file
+
 # ANSI escape code for green text
 GREEN = "\033[92m"
 RESET = "\033[0m"
@@ -12,24 +17,27 @@ RED = "\033[91m"
 if __name__ == "__main__":
     dir = sys.argv[1]
     model = sys.argv[2]
-    output = sys.argv[3]
+    outputLocation = sys.argv[3]
+    #TODO is correct here. If it already exist nothing happen, otherwise we are ok
+    os.makedirs(outputLocation, exist_ok=True)
+    
     samplesheet_not_found = False
-
+    added_files = 0
 
     #Look for existing samplesheet
     existing_samplesheet = list_json(dir)
     if len(existing_samplesheet) == 0:
         print(f"I wasn't able to find any existing samplesheet inside {GREEN}{dir}{RESET}")
         #Create a new samplesheet
-        samplesheet = Samplesheet(create_blank_samplesheet(dir, model))
+        samplesheet = Samplesheet(create_blank_samplesheet(dir, model, outputLocation))
         print("A {GREEN}new samplesheet was created{RESET}")
-        update_samplesheet(samplesheet)
+        added_files = update_samplesheet(samplesheet)
     else: 
         for sheet in existing_samplesheet:
-            if is_same_samplesheet(sheet, dir, model):
+            if is_same_samplesheet(sheet, dir, model, outputLocation):
                 print(f"An EXISTING samplesheet for {GREEN}{model}{GREEN} was found inside {GREEN}{dir}{RESET}")
                 samplesheet = Samplesheet(sheet)
-                update_samplesheet(samplesheet)
+                added_files = update_samplesheet(samplesheet)
                 samplesheet_not_found = False
                 break
             else:
@@ -37,7 +45,16 @@ if __name__ == "__main__":
 
     if samplesheet_not_found :    
         #Create a new samplesheet
-        samplesheet = Samplesheet(create_blank_samplesheet(dir, model))
+        samplesheet = Samplesheet(create_blank_samplesheet(dir, model, outputLocation))
         print(f"A {GREEN}new samplesheet{RESET} was created")
-        update_samplesheet(samplesheet)
+        added_files = update_samplesheet(samplesheet)
 
+    # Get the current date and time
+    current_datetime = datetime.now()
+    # Format the datetime to [DD/Mon/YYYY HH:MM:SS]
+    formatted_datetime = current_datetime.strftime("%d/%b/%Y %H:%M:%S")   
+    message = f"Scan of {dir} at {formatted_datetime} added {added_files} files to {samplesheet.file_path}"
+    telegram_send_bar(message)
+    
+    if added_files>0:
+        launch_run(samplesheet)
