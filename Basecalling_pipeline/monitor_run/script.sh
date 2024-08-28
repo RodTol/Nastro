ae6e9702a44ade6bdca5b72afdc5e0561e6c795e
RodTol <rodolfo.tolloi@gmail.com>
2024-08-28 14:30:43 +0200

    new venvs

--- Basecalling_pipeline/monitor_run/script.sh
+++ Basecalling_pipeline/monitor_run/script.sh
@@ -11,8 +11,12 @@
 pathToSamplesheet=$1
 RUN_PARAMS_PATH=$2
 
+source ~/python_venvs/orfeo_telegram_on_epyc/bin/activate
+
 python3 /u/area/jenkins_onpexp/LTS_tolloi/Orfeo_bot/main.py ${pathToSamplesheet} &
 pid=$!
+
+deactivate
 
 python3 main.py ${RUN_PARAMS_PATH} ${pathToSamplesheet} 
 
