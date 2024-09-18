fae78c1b382a41cda2f5708d9445260f5cbbe8ef
RodTol <rodolfo.tolloi@gmail.com>
2024-09-18 17:27:35 +0200

    Refactor project metadata and copyright information

--- Basecalling_pipeline/subset_creation/runParameters.py
+++ Basecalling_pipeline/subset_creation/runParameters.py
@@ -1,16 +1,12 @@
-# Copyright 2024 Area Science Park
-# Author: Rodolfo Tolloi
-#
-# Licensed under the Apache License, Version 2.0 (the "License");# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#__author__      ="Rodolfo Tolloi"
+#__subject__     ="Nastro Pipeline"
+#__tags__        ="Bioinformatics, Nanopore, Dorado"
+#__copyright__   ="Copyright 2021, AREA SCIENCE PARK - RIT"
+#__credits__     =
+#__license__     ="Apache License 2.0"
+#__version__     =
+#__maintainer__  =
+#__status__      ="Development"
 
 import os
 import json 
