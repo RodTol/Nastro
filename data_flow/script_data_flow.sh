aecadddd7e7fc25e1b8e72e2d89e462126924c53
RodTol <rodolfo.tolloi@gmail.com>
2024-09-18 14:47:06 +0200

    Test change source dir

--- data_flow/script_data_flow.sh
+++ data_flow/script_data_flow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2024 Rodolfo Tolloi
+# Copyright 2024 Area Science Park
+# Author: Rodolfo Tolloi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
