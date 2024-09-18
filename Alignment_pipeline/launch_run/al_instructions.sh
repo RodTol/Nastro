318a21428fedd8c78f7e99fd8ad2a46fcf6b7b82
RodTol <rodolfo.tolloi@gmail.com>
2024-09-18 15:27:29 +0200

    Refactor copyright and author information

--- Alignment_pipeline/launch_run/al_instructions.sh
+++ Alignment_pipeline/launch_run/al_instructions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Copyright 2024 Rodolfo Tolloi
-#
+# Copyright 2024 Area Science Park
+# Author: Rodolfo Tolloi
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
