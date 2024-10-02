2a5390cd1e67f7f091ed62e859531e0e74819b86
RodTol <rodolfo.tolloi@gmail.com>
2024-10-02 14:49:19 +0200

    force wait for sbatch in the pipeline for the merging, before proceeding

--- Alignment_pipeline/setupRun/merge_fastcat.sh
+++ Alignment_pipeline/setupRun/merge_fastcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=merge_fastcat
-#SBATCH --output=merge_fastcat.log
+#SBATCH --output=merge_fastcat.out
 #SBATCH --error=merge_fastcat.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -11,12 +11,13 @@
 
 fastq_pass_dir=$1
 merged_fastq_path=$2
+output_dir=$(dirname $fastq_pass_dir)
 
+source ~/.bashrc
 conda activate fastcat
 
 echo "Starting job on $(date)"
 
-fastcat $fastq_pass_dir > $merged_fastq_path
+fastcat --histograms=${output_dir}/fastcat_histograms $fastq_pass_dir > $merged_fastq_path
 
 echo "Job finished on $(date)"
-
