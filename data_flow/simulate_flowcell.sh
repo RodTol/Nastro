66d56c9119012ac26a493a8eddab41cb763c0f29
RodTol <rodolfo.tolloi@gmail.com>
2024-11-22 18:28:37 +0100

    Updated script for data sequencing simulation

--- data_flow/simulate_flowcell.sh
+++ data_flow/simulate_flowcell.sh
@@ -5,14 +5,24 @@ INPUT_FOLDER=$1/no_sample/*_${3}
 OUTPUT_FOLDER=$2/no_sample/*_${3}
 RUNTIME=$4
 
-#Compute frequency to cp .fast5 files to output dir
-NUM_FAST5=$(ls $INPUT_FOLDER/ | wc -l)
-PERIOD=$(python3 -c "print (abs(int(${RUNTIME}/${NUM_FAST5})))")
+NUM_POD5=$(ls $INPUT_FOLDER/*.pod5 | wc -l)
+NUM_FAST5=$(ls $INPUT_FOLDER/*.fast5 | wc -l)
 
+echo "Found $NUM_POD5 .pod5 files and $NUM_FAST5 .fast5 files in $INPUT_FOLDER"
+
+if [ $NUM_POD5 -gt $NUM_FAST5 ]; then
+        NUM_FILES=$NUM_POD5
+        FILE_EXTENSION="pod5"
+else
+        NUM_FILES=$NUM_FAST5
+        FILE_EXTENSION="fast5"
+fi
+
+PERIOD=$(python3 -c "print (abs(int(${RUNTIME}/${NUM_FILES})))")
 cd $INPUT_FOLDER
 
 # copy one file at a time, separated by $PERIOD seconds
-for i in $(ls *.fast5);
+for i in $(ls *.${FILE_EXTENSION});
 do
         cp --parents $i $OUTPUT_FOLDER/
         sleep ${PERIOD}s
--- data_flow/simulate_sequencing.sh
+++ data_flow/simulate_sequencing.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 
-if [ $# -eq 1 ] && [ -n ${1//[0-9]/} ]; then
-	PERIOD=$1
-	SRC_DIR=/orfeo/cephfs/scratch/area/jenkins_onpexp/experiment/HG002_src/
-	DST_DIR=/orfeo/cephfs/scratch/area/jenkins_onpexp/experiment/HG002_dst/
-	
-	mkdir -p $SRC_DIR
-	mkdir -p $DST_DIR
-	
-	# copy the directory structure to the destination folder 
-	# (before copying the individual files)
-	rsync -av -f"+ */" -f"- *" $SRC_DIR/ $DST_DIR/
-	
-	echo "Started sequencing at "$(date +%T)
-	# parallel processes, one for each flowcell to copy the files
-	# from the source to the destination folder at a uniform rate
-	# based on the total number of files in each folder,
-	# can vary from flowcell to flowcell
-	time parallel -j 6 \
+#SRC _DIR needs to be structured as in HG002_sample_data: so 
+# SRC_DIR/no_sample/*_{index}C 
+#where index is a value that goes from 1 to the N flowcell sets 
+
+# Check if 3 arguments are passed
+if [ $# -eq 3 ] && [ -n ${1//[0-9]/} ] && [ -d "$2" ] && [ -d "$3" ]; then
+    PERIOD=$1
+    SRC_DIR=$2
+    DST_DIR=$3
+    
+	#Number of simulated flowcells
+	FLOWCELLS=2
+
+    # Create destination directory if it doesn't exist
+    mkdir -p $DST_DIR
+    
+    # Copy the directory structure to the destination folder 
+    # (before copying the individual files)
+    rsync -av -f"+ */" -f"- *" $SRC_DIR/ $DST_DIR/
+    
+    echo "Started sequencing at "$(date +%T)
+    
+    # Parallel processes, one for each flowcell to copy the files
+    # from the source to the destination folder at a uniform rate
+    # based on the total number of files in each folder,
+    # can vary from flowcell to flowcell
+	time parallel -j $FLOWCELLS \
 		/orfeo/cephfs/scratch/area/jenkins_onpexp/experiment/simulate_flowcell.sh ::: \
 		$SRC_DIR ::: \
 		$DST_DIR ::: \
-		{1..6}C ::: \
+		{1..$FLOWCELLS}C ::: \  #each flowcell is called flowcell_1C, flowcell_2C, etc
 		$PERIOD
-	
-	# in case we want to simulate all 48 flow cells
-	#time parallel -j 48 $PROJECT_DIR/simulation/simulate_flowcell.sh ::: \
-	#       $SRC_DIR ::: \
-	#       $DST_DIR ::: \
-	#       {1..6}{A..H} ::: \
-	#       $PERIOD
-	exit 0
+    
+    # Uncomment below if you want to simulate all 48 flow cells
+    #time parallel -j 48 $PROJECT_DIR/simulation/simulate_flowcell.sh ::: \
+    #       $SRC_DIR ::: \
+    #       $DST_DIR ::: \
+    #       {1..6}{A..H} ::: \
+    #       $PERIOD
+
+    exit 0
 else
-	1>&2 echo "Error: Provided $# arguments" 
-        1>&2 echo "Need 1 input argument"
-        1>&2 echo "Usage: simulate_sequencing.sh simulation_duration_in_seconds"
-        exit 1
+    1>&2 echo "Error: Invalid arguments" 
+    1>&2 echo "Usage: simulate_sequencing.sh simulation_duration_in_seconds SRC_directory DST_directory"
+    exit 1
 fi
