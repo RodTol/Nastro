aff62998bc82883e8521a01932d8dc9f993d5709
RodTol <rodolfo.tolloi@gmail.com>
2024-11-23 17:11:13 +0100

    New pipeline for the simulation of the sequencing and processing

--- Simulation/simulate_sequencing.sh
+++ Simulation/simulate_sequencing.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#SBATCH --job-name=simulate_sequencing
+#SBATCH --time=03:00:00
+#SBATCH --output=simulate_sequencing.out
+#SBATCH --error=simulate_sequencing.err
+#SBATCH -A lage
+#SBATCH -p EPYC
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10GB
 
 #SRC _DIR needs to be structured as in HG002_sample_data: so 
 # SRC_DIR/no_sample/*_{index}C 
 #where index is a value that goes from 1 to the N flowcell sets 
+
+# CHECK FLOWECELLS, cpus_per_task and PARALLEL -j command
+# to have same argument!!!
 
 # Check if 3 arguments are passed
 if [ $# -eq 3 ]; then
@@ -10,8 +23,8 @@ if [ $# -eq 3 ]; then
     SRC_DIR=$2
     DST_DIR=$3
     
-	#Number of simulated flowcells
-	FLOWCELLS=2
+    # Number of simulated flowcells
+    FLOWCELLS=3
 
     echo "Simulation duration: $PERIOD seconds with $FLOWCELLS flowcells"
 
@@ -28,16 +41,15 @@ if [ $# -eq 3 ]; then
     # from the source to the destination folder at a uniform rate
     # based on the total number of files in each folder,
     # can vary from flowcell to flowcell
-    time parallel -j 2 \
+    time parallel -j 3 \
         "${HOME}/Nastro/Simulation/simulate_flowcell.sh" ::: \
         "$SRC_DIR" ::: \
         "$DST_DIR" ::: \
         {1..2}C ::: \
         "$PERIOD"
 
-    
     # Uncomment below if you want to simulate all 48 flow cells
-    #time parallel -j 48 $PROJECT_DIR/simulation/simulate_flowcell.sh ::: \
+    # time parallel -j 48 $PROJECT_DIR/simulation/simulate_flowcell.sh ::: \
     #       $SRC_DIR ::: \
     #       $DST_DIR ::: \
     #       {1..6}{A..H} ::: \
