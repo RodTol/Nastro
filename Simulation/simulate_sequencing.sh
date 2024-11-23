03bf3039e27b7cc907438d8580b7673f4c328557
RodTol <rodolfo.tolloi@gmail.com>
2024-11-23 22:16:09 +0100

    Increase job time and memory allocation in simulate_sequencing.sh

--- Simulation/simulate_sequencing.sh
+++ Simulation/simulate_sequencing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=simulate_sequencing
-#SBATCH --time=03:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=simulate_sequencing.out
 #SBATCH --error=simulate_sequencing.err
 #SBATCH -A lage
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=10GB
+#SBATCH --mem=50GB
 
 #SRC _DIR needs to be structured as in HG002_sample_data: so 
 # SRC_DIR/no_sample/*_{index}C 
